b8c1520acbb8f8711ca7b20551091c6824b5bc99
suiseki@gmail.com <suiseki@gmail.com>
2012-03-07 23:16:34 -0800

    adding extention check

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express');
-
+var http = require("http"), 
+	querystring = require("querystring"),
+	path = require("path"), 
+	fs = require("fs")
+	extensions = {".html": "text/html",
+	".css": "text/css",
+	".js": "application/javascript",
+	".png": "image/png",
+	".gif": "image/gif",
+	".jpg": "image/jpeg"
+	};
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
