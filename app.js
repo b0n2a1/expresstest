43e16e2413913de4242519b11d6f46732bc1b2b8
suiseki@gmail.com <suiseki@gmail.com>
2012-03-07 23:27:59 -0800

    add file parsing

--- app.js
+++ app.js
@@ -12,9 +12,36 @@ var http = require("http"),
 	};
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+	http.createServer(function(req, res) { 
+		var filename = path.basename(req.url) || "index.html", 
+		ext = path.extname(filename),
+		dir = path.dirname(req.url).substring(1),
+		localPath = __dirname + "/public/";
+		if (extensions[ext]) { 
+			localPath += (dir ? dir + "/" : "") + filename; 
+			path.exists(localPath, function(exists) { 
+					if (exists) { 
+						getFile(localPath, extensions[ext], res); 
+					} else { 
+						res.writeHead(404); res.end(); } 
+					}); 
+				} 
+				}).listen(8000);
+
+function getFile(localPath, mimeType, res){
+	fs.readFile(localPath, function(err, contents){
+		if(!err){
+			res.writeHead(200,{
+				"Content-Type": mimeType,
+				"Content-Length": contents.length
+			});
+			res.end(contents);
+		}else{
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
