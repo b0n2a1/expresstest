e6c68adebdef0425046286665990efc789f4ed3c
suiseki@gmail.com <suiseki@gmail.com>
2012-03-08 20:47:34 -0800

    starting over

--- app.js
+++ app.js
@@ -1,49 +1,7 @@
-var express = require('express');
-var http = require("http"), 
-	querystring = require("querystring"),
-	path = require("path"), 
-	fs = require("fs")
-	extensions = {".html": "text/html",
-	".css": "text/css",
-	".js": "application/javascript",
-	".png": "image/png",
-	".gif": "image/gif",
-	".jpg": "image/jpeg"
-	};
-var app = express.createServer(express.logger());
+var app = require('express').createServer();
 
-	app.get(function(req, res) { 
-		var filename = path.basename(req.url) || "index.html", 
-		ext = path.extname(filename),
-		dir = path.dirname(req.url).substring(1),
-		localPath = __dirname + "/public/";
-		if (extensions[ext]) { 
-			localPath += (dir ? dir + "/" : "") + filename; 
-			path.exists(localPath, function(exists) { 
-					if (exists) { 
-						getFile(localPath, extensions[ext], res); 
-					} else { 
-						res.writeHead(500); res.end(); } 
-					}); 
-				} 
-				});
-
-function getFile(localPath, mimeType, res){
-	fs.readFile(localPath, function(err, contents){
-		if(!err){
-			res.writeHead(200,{
-				"Content-Type": mimeType,
-				"Content-Length": contents.length
-			});
-			res.end(contents);
-		}else{
-			res.writeHead(500);
-			res.end();
-		}
-	});
-}
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
+
+app.listen(process.env.PORT || 3000);
