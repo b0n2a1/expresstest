eb9ddc57d3fd0a73c0e0ba891f5c24e4c7fa55e0
suiseki@gmail.com <suiseki@gmail.com>
2012-03-08 20:59:49 -0800

    more code

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ var http = require("http"),
 var app = require('express').createServer();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	writecontent();
 });
+
+function writecontent(){
+  response.send('Hello World!');
+}
 
 app.listen(process.env.PORT || 3000);
