bdf2e12cf9b10d62c745f59ef232f2abf37faa78
suiseki@gmail.com <suiseki@gmail.com>
2012-03-08 21:18:30 -0800

    commit

--- app.js
+++ app.js
@@ -16,7 +16,19 @@ app.get('/', function(request, response) {
 });
 
 function writecontent(request, response){
-  response.send('Hello World!');
+	var filename = path.basename(req.url) || "index.html", 
+	ext = path.extname(filename),
+	dir = path.dirname(req.url).substring(1),
+	localPath = __dirname + "/public/";
+	if (extensions[ext]) { 
+		localPath += (dir ? dir + "/" : "") + filename; 
+		path.exists(localPath, function(exists) { 
+				if (exists) { 
+					getFile(localPath, extensions[ext], res); 
+				} else { 
+					res.writeHead(404); res.end(); } 
+				}); 
+			}
 }
 
 app.listen(process.env.PORT || 3000);
