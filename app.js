f7a75f7a68a352fc1ecd6590b17771af54e8506b
suiseki@gmail.com <suiseki@gmail.com>
2012-03-08 20:51:21 -0800

    add extension hash

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var http = require("http"), 
+	querystring = require("querystring"),
+	path = require("path"), 
+	fs = require("fs")
+	extensions = {".html": "text/html",
+	".css": "text/css",
+	".js": "application/javascript",
+	".png": "image/png",
+	".gif": "image/gif",
+	".jpg": "image/jpeg"
+	};
 var app = require('express').createServer();
 
 app.get('/', function(request, response) {
