165ea2a19f29da43e3ab50d1536f1cb973008cbc
suiseki@gmail.com <suiseki@gmail.com>
2012-03-09 22:41:10 -0800

    get twitter

--- app.js
+++ app.js
@@ -14,9 +14,23 @@ app.get('/', function(req,res){
 });
 
 app.get('/twit/', function(req,res){
-	var twitstream = "This is a json stream";
+	var twitstream = getTweets();//"This is a json stream";
+	
+	
+	
 	res.json(twitstream);
-})
+});
+
+function getTweets(){
+	var request = http.request({
+		host: "search.twitter.com",
+		method: "GET",
+		path: "/search.json?q=b0n2a1"
+	})
+	return request;
+}
+
+
 
 app.listen(process.env.PORT || 3000);
 
