f4c71ea45f6cd10caf2aa5ed87f77de6de0ad96f
suiseki@gmail.com <suiseki@gmail.com>
2012-03-14 22:34:25 -0700

    a

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.get('/twit/', function(req,res){
 //	req.write('data\n');
 //	req.write('data\n');
 //	req.end();
-	res.write('Hello');
+	req.write('Hello');
+	req.end();
 //	t.search('@b0n2a1 OR #poulsbo', function(err, data) {
 //	  if (err) {
 //	    console.log('Twitter search failed!');
