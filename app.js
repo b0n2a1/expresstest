84ca5fbc0d301dc954a4e5cde8d8a19c688fe6bb
suiseki@gmail.com <suiseki@gmail.com>
2012-03-14 20:42:00 -0700

    a

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-var twitter = require('ntwitter');
-var credentials = require('./credentials.js');
 var express = require('express');
 var fs = require('fs');
 var util = require('util');
 var http = require('http');
 
-var t = new twitter({
-    consumer_key: credentials.consumer_key,
-    consumer_secret: credentials.consumer_secret,
-    access_token_key: credentials.access_token_key,
-    access_token_secret: credentials.access_token_secret
+var app = express.createServer();
+app.configure('development', function(){
+	app.use(express.logger());
+	app.use(express.static(__dirname + '/public'));
 });
 
-//	t.stream(
-//	    'statuses/filter',
-//	    { track: ['@b0n2a1', '@szich', '@darrylhan','bonsai','wow'] },
-//	    function(stream) {
-//	        stream.on('data', function(tweet) {
-//	            console.log(tweet.text);
-//				response.write(JSON.stringify(tweet.text));
-//	        });
-//	    }
-//	);
-
-t.search('@b0n2a1 OR #poulsbo', function(err, data) {
-  if (err) {
-    console.log('Twitter search failed!');
-  }
-  else {
-    //console.log('Search results:');
-    //console.dir(data);
-	JSON.stringify(data);
-//	response.write(data);
-  }
+app.get('/', function(req,res){
+	var streamIn = fs.createReadStream(__dirname + '/public/index.html');
+	util.pump(streamIn, res);
 });
+
+app.get('/twit/', function(req,res){
+	//return JSON.parse(request);
+	var options = {
+		host: "search.twitter.com",
+		method: "GET",
+		path: "/search.json?q=b0n2a1"	
+	}
+	
+	var req = http.get(options, function(res){
+		console.log('status:' + res.statusCode);
+		console.log('headers:' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (chunk){
+			console.log('BODY:' + chunk);
+		});
+	});
+	req.write('data\n');
+	req.write('data\n');
+	req.end();
+
+});
+
+function getTweets(){
+	var request = http.request({
+		host: "http://search.twitter.com",
+		method: "GET",
+		path: "/search.json?q=b0n2a1"
+	})
+	return JSON.parse(request);	
+}
+
+app.listen(process.env.PORT || 3000);
+
 
 
