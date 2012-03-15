fc5029d5d9da93a066c25aea3c9588167e87fc13
suiseki@gmail.com <suiseki@gmail.com>
2012-03-14 20:48:48 -0700

    s

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+var twitter = require('ntwitter');
+var credentials = require('./credentials.js');
 var express = require('express');
 var fs = require('fs');
 var util = require('util');
 var http = require('http');
+
+var t = new twitter({
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
+});
 
 var app = express.createServer();
 app.configure('development', function(){
@@ -16,10 +25,21 @@ app.get('/', function(req,res){
 
 app.get('/twit/', function(req,res){
 	//return JSON.parse(request);
-	var options = {
-		host: "search.twitter.com",
-		method: "GET",
-		path: "/search.json?q=b0n2a1"	
+//	var options = {
+//		host: "search.twitter.com",
+//		method: "GET",
+//		path: "/search.json?q=b0n2a1"	
+		t.search('@b0n2a1 OR #poulsbo', function(err, data) {
+		  if (err) {
+		    console.log('Twitter search failed!');
+		  }
+		  else {
+		    //console.log('Search results:');
+		    //console.dir(data);
+		//		JSON.stringify(data);
+			response.write(data);
+		  }
+		});
 	}
 	
 	var req = http.get(options, function(res){
